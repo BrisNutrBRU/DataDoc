6c11ef42d06e871ada5689aac1fe06b3d6303352
Chris-M-P <chris.penfold@bristol.ac.uk>
2015-08-18 14:58:22 +0100

    Update

--- R/AFAST Offspring/AFAST Offspring.R
+++ R/AFAST Offspring/AFAST Offspring.R
@@ -1,7 +1,7 @@
 library(RODBC)
 
 db <- "G:/Research/BRU Nutrition/BRU Group/Core theme/Data documentation project/AFAST Offspring/AFAST DB BE.accdb"
-con2 <- odbcConnectAccess2007(db)
+con2 <- odbcConnectAccess2007(db) # you may need to set-up your own connection to this database in Windows
 AFASTQ <- sqlFetch(con2, "tblQuestionnaireData")
 
 odbcCloseAll()
--- R/HN5000/HN5000.R
+++ R/HN5000/HN5000.R
@@ -3,7 +3,5 @@ library(readstata13)
 hn5000.dta <- read.dta13("G:/Research/BRU Nutrition/BRU Group/Core theme/Data documentation project/H&N 5000/test subset.dta", convert.factors = TRUE)
 hn5000.csv <- read.csv("G:/Research/BRU Nutrition/BRU Group/Core theme/Data documentation project/H&N 5000/test subset.csv", header=TRUE)
 
-View(hn5000.dta)
-View(hn5000.csv)
-
-Print("This is a test")
+# View(hn5000.dta)
+# View(hn5000.csv)
